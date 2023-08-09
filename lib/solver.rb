f119b4d4ec4fa07e448c3142daac295f606c12ca
Sandeep Ghosh <mailsg0912@gmail.com>
2023-08-09 15:32:11 +0530

    Add test case for reverse word

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,7 +1,8 @@
 class Solver
-    def factorial(n)
-        raise ArgumentError, "Argument should be a non-negative integer" if n < 0
-        return 1 if n == 0
-        (1..n).reduce(:*)
-    end
+  def factorial(n)
+    raise ArgumentError, 'Argument should be a non-negative integer' if n.negative?
+    return 1 if n.zero?
+
+    (1..n).reduce(:*)
+  end
 end
